4db1c9ece982950c2b97a1e43caa3dd13359cf9c
yeyouluo <yeyouluogg@gmail.com>
2024-05-19 23:08:46 +0800

    update from vitepress: 2024-05-19 23:08:46

---
+++ assets/base_linux_linux-vim.md.Bni-SGN5.lean.js
@@ -0,0 +1,1 @@
+import{_ as e,c as t,o as a}from"./chunks/framework.biRBrEtS.js";const u=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"base/linux/linux-vim.md","filePath":"base/linux/linux-vim.md"}'),n={name:"base/linux/linux-vim.md"};function i(s,r,o,c,l,m){return a(),t("div")}const _=e(n,[["render",i]]);export{u as __pageData,_ as default};
